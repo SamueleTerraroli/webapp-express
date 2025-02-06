de5528ceaff87ba099c6ddf59827340f40583153
SamueleTerraroli <terraroli99@gmail.com>
2025-02-06 15:34:04 +0100

    feat: add image path

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -7,7 +7,13 @@ const index = (req, res) => {
 
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Query error on database' });
-        res.json(results);
+
+        const moviesWithImages = results.map(movie => ({
+            ...movie,
+            image_url: movie.image ? `/${movie.image}` : null
+        }));
+
+        res.json(moviesWithImages);
     })
 
 }
@@ -15,40 +21,46 @@ const index = (req, res) => {
 //show root
 const show = (req, res) => {
     const id = req.params.id;
-    const sql = `SELECT M.*, ROUND(AVG(R.vote),1) AS average_vote
-    FROM movies M
-    LEFT JOIN reviews R ON M.id = R.movie_id
-    WHERE M.id = ?
-    `
+    const sql = `SELECT M.*, ROUND(AVG(R.vote),1) AS average_vote, M.image
+                 FROM movies M
+                 LEFT JOIN reviews R ON M.id = R.movie_id
+                 WHERE M.id = ?`;
+
     const sqlReviews = `SELECT *
-    FROM reviews R
-    WHERE R.movie_id = ?
-    `
-    //movie query
+                        FROM reviews R
+                        WHERE R.movie_id = ?`;
+
+    // movie query
     connection.query(sql, [id], (err, results) => {
         if (err) {
             console.error('Movie Query Error:', err);
             return res.status(500).json({ error: 'Query error on database' });
         }
-        if (results.length === 0 || results[0].id === null) return res.status(404).json({ error: 'Movie not found' });
 
-        //reviws query
+        if (results.length === 0 || results[0].id === null) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        const movie = {
+            ...results[0],
+            image_url: results[0].image ? `/${results[0].image}` : null
+        };
+
+        // reviews query
         connection.query(sqlReviews, [id], (err, resultsReviews) => {
             if (err) {
                 console.error('Reviews Query Error:', err);
                 return res.status(500).json({ error: 'Query error on database' });
             }
 
-            const movie = results[0];
             res.json({
                 ...movie,
                 reviews: resultsReviews
             });
-        })
+        });
+    });
+};
 
-
-    })
-}
 
 module.exports = {
     index,
