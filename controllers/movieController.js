a15fb2b218a23e8b8760a3c2c6a482a99dbc8fec
SamueleTerraroli <terraroli99@gmail.com>
2025-02-06 13:29:41 +0100

    feat: add base root in controller

---
+++ controllers/movieController.js
@@ -0,0 +1,16 @@
+//index root
+const index = (req, res) => {
+    res.sen('Movie list');
+
+}
+
+//show root
+const show = (req, res) => {
+    const id = req.params.id;
+    res.send(`Movie detail on id ${id}`)
+}
+
+module.exports = {
+    index,
+    show
+}
